411341deccf66aba61aa891fdd40bdb2f6c9a541
Dioneuty <dioneuty@gmail.com>
2017-05-26 16:21:30 +0900

    new model and simple vo

--- maven/src/main/java/com/hb/maven/controller/ListController.java
+++ maven/src/main/java/com/hb/maven/controller/ListController.java
@@ -1,11 +1,13 @@
 package com.hb.maven.controller;
 
 import java.io.IOException;
+import java.sql.SQLException;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.hb.maven.model.SimpleDao;
 import com.hb.maven.support.MyController;
 
 public class ListController implements MyController{
@@ -13,6 +15,12 @@ public class ListController implements MyController{
 	@Override
 	public String execute(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+		SimpleDao dao = new SimpleDao();
+		try {
+			request.setAttribute("alist", dao.selectAll());
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 		return "list";
 	}
 
