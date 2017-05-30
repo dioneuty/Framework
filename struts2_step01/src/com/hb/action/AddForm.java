105f4711ae487aec499c78a68ff155b983f6863e
Dioneuty <dioneuty@gmail.com>
2017-05-30 16:16:11 +0900

    add, delete, edit, insdert

---
+++ struts2_step01/src/com/hb/action/AddForm.java
@@ -0,0 +1,10 @@
+package com.hb.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class AddForm extends ActionSupport{
+	@Override
+	public String execute() throws Exception {
+		return super.execute();
+	}
+}
---
+++ struts2_step01/src/com/hb/action/UpdateOne.java
@@ -0,0 +1,10 @@
+package com.hb.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class UpdateOne extends ActionSupport{
+	@Override
+	public String execute() throws Exception {
+		return super.execute();
+	}
+}
