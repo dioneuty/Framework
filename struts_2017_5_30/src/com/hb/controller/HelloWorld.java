569b340b8506a661dc1cb2c33f535523271b4c33
Dioneuty <dioneuty@gmail.com>
2017-05-30 11:22:53 +0900

    struts

---
+++ struts_2017_5_30/src/com/hb/controller/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.hb.controller;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class HelloWorld extends ActionSupport{
+	@Override
+	public String execute() throws Exception {
+		System.out.println("dfdkfd");
+		return super.execute();
+		
+	}
+}
