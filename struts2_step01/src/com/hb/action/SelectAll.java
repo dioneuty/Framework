2e6f9e79ad2d620c4bcd3b76b36f9bd5236ef21f
Dioneuty <dioneuty@gmail.com>
2017-05-30 14:37:32 +0900

    first commit

---
+++ struts2_step01/src/com/hb/action/SelectAll.java
@@ -0,0 +1,10 @@
+package com.hb.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class SelectAll extends ActionSupport{
+	@Override
+	public String execute() throws Exception {
+		return this.SUCCESS;
+	}
+}
