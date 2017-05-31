773b81ca605a415065d9efcd7647c1e7cd885f7b
Dioneuty <dioneuty@gmail.com>
2017-05-31 14:39:56 +0900

    modify

--- step02/src/main/java/com/hb/struts/action/InsertTwoAction.java
+++ step02/src/main/java/com/hb/struts/action/InsertTwoAction.java
@@ -1,13 +1,21 @@
 package com.hb.struts.action;
 
+import com.hb.struts.model.SimpleDao;
+import com.hb.struts.model.SimpleVo;
 import com.opensymphony.xwork2.Action;
 
 public class InsertTwoAction implements Action {
-
+	SimpleVo bean = new SimpleVo();
+	
 	@Override
 	public String execute() throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		SimpleDao dao = new SimpleDao();
+		//int result = dao.insertOne(sabun,name,pay);
+		SimpleVo bean = new SimpleVo();
+		int result = dao.insertOne(bean);
+		if(result>0) return SUCCESS;
+		else return INPUT;
 	}
+
 
 }
