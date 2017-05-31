0f0396345f12523752ec9562b140f8303e12344b
Dioneuty <dioneuty@gmail.com>
2017-05-31 14:37:34 +0900

    efe

--- step02/src/main/java/com/hb/struts/action/InsertOneAction.java
+++ step02/src/main/java/com/hb/struts/action/InsertOneAction.java
@@ -1,28 +1,21 @@
 package com.hb.struts.action;
 
 import com.hb.struts.model.SimpleDao;
+import com.hb.struts.model.SimpleVo;
 import com.opensymphony.xwork2.Action;
 
 public class InsertOneAction implements Action {
-	private int sabun,pay;
-	private String name;
-	
-	public void setSabun(int sabun) {
-		this.sabun = sabun;
-	}
-	
-	public void setPay(int pay) {
-		this.pay = pay;
-	}
-	
-	public void setName(String name) {
-		this.name = name;
-	}
+	SimpleVo bean = new SimpleVo();
 	
 	@Override
 	public String execute() throws Exception {
 		SimpleDao dao = new SimpleDao();
-		int result = dao.insertOne(sabun,name,pay);
+		//int result = dao.insertOne(sabun,name,pay);
+		SimpleVo bean = new SimpleVo();
+		bean.setSabun(sabun);
+		bean.setName(name);
+		bean.setPay(pay);
+		int result = dao.insertOne(bean);
 		if(result>0) return SUCCESS;
 		else return INPUT;
 	}
