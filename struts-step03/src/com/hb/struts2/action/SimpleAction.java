70c383f6c6744332cb3ad70dd9d7d33b60570c57
Dioneuty <dioneuty@gmail.com>
2017-06-01 10:58:52 +0900

    list

---
+++ struts-step03/src/com/hb/struts2/action/SimpleAction.java
@@ -0,0 +1,16 @@
+package com.hb.struts2.action;
+
+public class SimpleAction {
+	public String list(){
+		return "success";
+	}
+	public String add(){
+		return "success";
+	}
+	public String detail(){
+		return "success";
+	}
+	public String edit(){
+		return "success";
+	}
+}
