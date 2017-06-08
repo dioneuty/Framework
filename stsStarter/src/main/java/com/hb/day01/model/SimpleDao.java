fc233607f376eeb30d4571b713401d3a65bcd8ba
Dioneuty <dioneuty@gmail.com>
2017-06-08 16:25:26 +0900

    update, edit, add @annotation

---
+++ stsStarter/src/main/java/com/hb/day01/model/SimpleDao.java
@@ -0,0 +1,105 @@
+package com.hb.day01.model;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.hb.day01.util.MyOracle;
+
+public class SimpleDao {
+	private Connection conn;
+	private PreparedStatement pstmt;
+	private ResultSet rs;
+
+	public List<SimpleVo> selectAll() throws SQLException{
+		String sql = "SELECT * FROM simple03 ORDER BY sabun";
+		List<SimpleVo> list = null;
+		try {
+			conn = MyOracle.getConn();
+			pstmt = conn.prepareStatement(sql);
+			rs = pstmt.executeQuery();
+			//자원 절약
+			list = new ArrayList<SimpleVo>(); 
+			while (rs.next()) {
+				list.add(new SimpleVo(rs.getInt("sabun")
+						, rs.getString("name")
+						, rs.getDate("nalja")
+						, rs.getInt("pay")
+						));
+			}
+		} finally {
+			closeAll();
+		}
+		return list;
+	}
+
+	private void closeAll() throws SQLException {
+		if(rs != null) rs.close();
+		if(pstmt != null) pstmt.close();
+		if(conn != null) conn.close();
+	}
+
+	public void insertOne(int sabun, String name, int pay) throws SQLException {
+		String sql = "insert into simple03 values (?,?,sysdate,?)";
+		try {
+			conn = MyOracle.getConn();
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, sabun);
+			pstmt.setString(2, name);
+			pstmt.setInt(3, pay);
+			pstmt.executeUpdate();
+		} finally {
+			closeAll();
+		}
+	}
+
+	public SimpleVo detailOne(int num) throws SQLException {
+		String sql = "select * from simple03 where sabun=?";
+		SimpleVo bean = null;
+		try {
+			conn = MyOracle.getConn();
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, num);
+			rs = pstmt.executeQuery();
+			if(rs.next()){
+				bean = new SimpleVo(rs.getInt("sabun")
+						, rs.getString("name")
+						, rs.getDate("nalja")
+						, rs.getInt("pay")
+						);
+			}
+		} finally {
+			closeAll();
+		}
+		return bean;
+	}
+
+	public int updateOne(int sabun, String name, int pay) throws SQLException {
+		String sql = "update simple03 set name=?, nalja=sysdate, pay=? where sabun=?";
+		try {
+			conn = MyOracle.getConn();
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, name);
+			pstmt.setInt(2, pay);
+			pstmt.setInt(3, sabun);
+			return pstmt.executeUpdate();
+		} finally {
+			closeAll();
+		}
+	}
+
+	public int deleteOne(int sabun) throws SQLException {
+		String sql = "delete from simple03 where sabun=?";
+		try {
+			conn = MyOracle.getConn();
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, sabun);
+			return pstmt.executeUpdate();
+		} finally {
+			closeAll();
+		}
+	}
+}
--- stsStarter/src/main/java/com/hb/day01/test01/Test04.java
+++ stsStarter/src/main/java/com/hb/day01/test01/Test04.java
@@ -18,33 +18,40 @@ public class Test04 {
 		model.addAttribute("msg","¸ðµ¨·Î Àü´Þ");
 		return "ex02";
 	}
+	
 	@RequestMapping("/ex05")
 	public String myPage02(){
 		return "ex01";
 	}
+	
 	@RequestMapping("/ex06")
 	public void myPage03(){
 	}
+	
 	@RequestMapping("/ex07")
 	public String myPage04(Model model, HttpServletRequest req) {
 		System.out.println(req.getParameter("msg"));
 		model.addAttribute("msg", req.getParameter("msg"));
 		return "ex07";
 	}
+	
 	@RequestMapping("/ex08")
 	public String myPage05(@RequestParam("msg") String msg){
 		System.out.println(msg);
 		return "ex08";
 	}
+	
 	@RequestMapping("path/{a}")
 	public String myPage06(@PathVariable String a) {
 		System.out.println("a:" + a);
 		return "ex01";
 	}
+	
 	@RequestMapping("/ex09")
 	public String myPage09(){
 		return "ex02";
 	}
+	
 	@RequestMapping(value = "/ex10", method = RequestMethod.POST)
 	public String myPage10(String id, String pw) {
 		System.out.println("id: " + id + ", pw: " + pw);
