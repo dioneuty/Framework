c97444f46a85f65b06cad5f2f2e0febc88aef191
Dioneuty <dioneuty@gmail.com>
2017-06-13 14:42:11 +0900

    start

---
+++ stsFive/src/main/java/com/hb/day04pm/GuestDaoImpl1.java
@@ -0,0 +1,61 @@
+package com.hb.day04pm;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.SQLException;
+import java.util.List;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import com.hb.day04pm.model.dto.GuestVo;
+
+public class GuestDaoImpl1 implements GuestDao {
+	
+	private SqlSessionFactory sqlSessionFactory;
+
+	public GuestDaoImpl1() throws IOException {
+		String resource = "./mybatis-config.xml";
+		
+		InputStream inputStream;
+		try {
+			inputStream = Resources.getResourceAsStream(resource);
+			sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+			
+		} catch (Exception e) {
+			System.out.println("config파일 리딩 오류");
+		}
+	}
+	
+	@Override
+	public List<GuestVo> selectAll() throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public GuestVo selectOne(int sabun) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void insertOne(GuestVo bean) throws SQLException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public int updateOne(GuestVo bean) throws SQLException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int deleteOne(int sabun) throws SQLException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
